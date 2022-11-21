4f46cfc8adf1d817b48d1e3618662149699d8361
Your Name <you@example.com>
2022-11-20 21:58:42 -0800

    fixed typo

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -22,7 +22,7 @@ function Projects() {
                             <h3>MEETING SCHEDULE TRACKER</h3>
                             <h3 className="tech-used">TECHNOLOGIES: TypeScript, Node.js, Express, MongoDB (NoSQL), React, HTML, CSS</h3>
                             <p className="project-description">
-                                Ever felt frustrated trying to schedule a meeting that fits everyone's availabilities? This is an issue I've often ran into. I love solutions such as the calendar interface on Microsoft Teams when it comes to displaying and scheduling meetings. The caveat is that it's tedious to schedule a meeting that lines up with the availabilities of 5 other people in 3 different timezones. I created an app that allows a user to create, schedule, and view others' meeting times. Now, if you want to schedule a meeting on Wednesday with Alice and John, you can check their schedule on the calendar as well.
+                                Ever felt frustrated trying to schedule a meeting that fits everyone's availabilities? This is an issue I've often run into. I love solutions such as the calendar interface on Microsoft Teams when it comes to displaying and scheduling meetings. The caveat is that it's tedious to schedule a meeting that lines up with the availabilities of 5 other people in 3 different timezones. I created an app that allows a user to create, schedule, and view others' meeting times. Now, if you want to schedule a meeting on Wednesday with Alice and John, you can check their schedule on the calendar as well.
                             </p>
                         </div>
                     </div>
